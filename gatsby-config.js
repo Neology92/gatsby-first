3b6752fd323de1926148793d30c569af8bf281fb
Neology92 <oskarlegner@gmail.com>
2019-05-31 01:38:06 +0200

    Add styled components plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-resolve-src', // Absolute path in code
+    `gatsby-plugin-styled-components`, // Styled Components <3
     `gatsby-transformer-remark`, //
     `gatsby-plugin-react-helmet`,
     {

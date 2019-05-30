19e49901c07b99b17583c94561cdb6cb4aec9175
Neology92 <oskarlegner@gmail.com>
2019-05-30 05:38:58 +0200

    Use post's path as key in list

--- src/components/Archive.js
+++ src/components/Archive.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 
+const Archive = () => (
+  <StaticQuery
+    query={POST_ARCHIVE_QUERY}
+    render={({ allMarkdownRemark }) => (
+      <>
+        <aside>
+          <h3>Archive</h3>
+          <ul>
+            {allMarkdownRemark.edges.map(edge => (
+              <li key={edge.node.frontmatter.path}>
+                {edge.node.frontmatter.title}
+              </li>
+            ))}
+          </ul>
+        </aside>
+      </>
+    )}
+  />
+);
+
 const POST_ARCHIVE_QUERY = graphql`
   query BlogPostsArchive {
     allMarkdownRemark {
@@ -15,23 +35,4 @@ const POST_ARCHIVE_QUERY = graphql`
     }
   }
 `;
-
-const Archive = () => (
-  <StaticQuery
-    query={POST_ARCHIVE_QUERY}
-    render={({ allMarkdownRemark }) => (
-      <>
-        <aside>
-          <h3>Archive</h3>
-          <ul>
-            {allMarkdownRemark.edges.map((edge, index) => (
-              <li key={index}>{edge.node.frontmatter.title}</li>
-            ))}
-          </ul>
-        </aside>
-      </>
-    )}
-  />
-);
-
 export default Archive;

038279fe41de6d57288d1b879dcb0a3f0f45b28c
Neology92 <oskarlegner@gmail.com>
2019-05-31 21:48:33 +0200

    Play with meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Roll 4 Design`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Roll 4 Gatsby Blog`,
+    description: `My first gatsby project. Let's learn some new things!`,
+    author: `@Neology`,
   },
   plugins: [
     'gatsby-plugin-resolve-src', // Absolute path in code
